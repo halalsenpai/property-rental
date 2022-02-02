52c39faacafec593ea658e384a09de863fbb8f7e
halalsenpai <halalsenpai@gmail.com>
2022-02-02 21:28:26 +0500

    icons added

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -15,12 +15,12 @@ export const findIcon = (keyword) => {
       return "house-user";
 
     case "Investment":
-      return "chart-line";
+      return "chart-line text-success";
 
     case "Cash_Only":
       return "euro-line";
 
-    case "Auction":
+    case "Online_Auction":
       return "gavel text-primary";
 
     case "Auction":
@@ -34,6 +34,48 @@ export const findIcon = (keyword) => {
 
     case "Empty":
       return "empty";
+
+    case "Knotweed":
+      return "knotweed";
+    // TODO
+    case "Listed":
+      return "";
+
+    case "New_Home":
+      return "home text-success";
+
+    case "Refurb":
+      return "hammer";
+
+    case "Probate":
+      return "file-contract text-success";
+
+    case "Reposessed":
+      return "ban text-danger";
+
+    case "Retirement":
+      return "retirement";
+
+    case "Shared":
+      return "handshake";
+
+    case "Shared_Ownership":
+      return "shared-ownership";
+
+    case "Tenanted":
+      return "tenanted";
+
+    case "Portfolio":
+      return "portfolio";
+
+    case "Plot":
+      return "drafting-compass";
+
+    case "Online_Viewing":
+      return "online-viewing";
+    
+    case "Full_Planning":
+      return "full-planning"
   }
 };
 
